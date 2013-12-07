da2e8811c62ec36496603e8bc4ea8dde585213c7
W. Andrew Loe III <andrew@andrewloe.com>
2013-12-06 17:07:09 -0800

    Use an environment variable so we can point to private data.

--- test/transmogriffy_test.rb
+++ test/transmogriffy_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 class ImporterTest < Minitest::Test
   def setup
-    @i = Transmogriffy::Importer.new(:lighthouse_export_path => './americano')
+    @i = Transmogriffy::Importer.new(:lighthouse_export_path => ENV['LIGHTHOUSE_EXPORT_PATH'])
   end
 
   def test_it_should_accept_options

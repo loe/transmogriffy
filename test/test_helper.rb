f73b9e1b7e9cbacbb937dbb63be0d4195833e7db
W. Andrew Loe III <andrew@andrewloe.com>
2013-12-22 12:26:34 -0800

    Configure VCR.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,8 @@
 require 'minitest/autorun'
+require 'vcr'
 require File.expand_path('../../lib/transmogriffy', __FILE__)
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/cassettes'
+  c.hook_into :faraday # Octokit is built on faraday.
+end
